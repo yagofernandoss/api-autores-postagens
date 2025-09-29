d393f6e3f8f1a42af2810a42aa7ff4c77f8e097d
Yago Fernando <yagofernandoss19@gmail.com>
2025-09-29 15:16:51 -0300

    implementação do banco de dados

---
+++ src/modelos/Bancodedados.ts
@@ -0,0 +1,5 @@
+import Autor from "./Autor";
+import Post from "./post";
+
+export const autores : Autor[] = []
+export const posts : Post [] = []
--- src/modelos/Post.ts
+++ src/modelos/Post.ts
@@ -1,21 +1,26 @@
 import {v4 as uuidv4} from 'uuid'
+import Autor from './Autor'
 
-type Tautor = {
-    nome: string
-    idade: number
+type Tpost = {
+    titulo: string
+    descricao: string
+    autor: Autor
 }
 
-export default class Autor {
+export default class Post {
     readonly id: string
-    nome: string
-    idade: number
-
-    constructor (autor:Tautor){
+    titulo: string
+    descricao: string
+    autor: Autor
+    data_criacao: Date
+    
+    constructor (post:Tpost){
         this.id = this.gerarId()
-        this.nome = autor.nome
-        this.idade = autor.idade
-
-
+        this.titulo = post.titulo
+        this.descricao = post.descricao
+        this.autor = post.autor
+        this.data_criacao = new Date()
+   
     }
 
     private gerarId(): string{
